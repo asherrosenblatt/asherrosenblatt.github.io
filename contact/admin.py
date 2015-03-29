8153c43a05b79ff33e6009d853643eb00b2138e5
asherrosenblatt <asher.rosenblatt@me.com>
2015-03-28 21:24:44 -0400

    removed bad files

--- contact/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import SignUp
-
-class SignUpAdmin(admin.ModelAdmin):
-    class Meta:
-        model = SignUp
-        
-admin.site.register(SignUp, SignUpAdmin)
